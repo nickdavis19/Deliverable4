916fcd76776db768d1b8e35bce0b7088cdef37e4
erh74 <erh74@pitt.edu>
2017-11-05 22:48:06 -0500

    hashit and readfile unit tests

--- Deliverable4Test.java
+++ Deliverable4Test.java
@@ -65,10 +65,46 @@ public class Deliverable4Test {
     //call the hashIt function with a misformatted file (specifically does not have any :'s in it)
     d4.hashIt("el.txt");
 
-    // assertTrue(outContent.toString().contains("Unable to read the file: el.txt"));
-    assertEquals(outContent.toString(), "Unable to hash the file: el.txt");
+    assertTrue(outContent.toString().contains("Unable to hash the file:"));
+    // assertEquals(outContent.toString(), "Unable to hash the file: el.txt");
 
   }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Test the readFile function with a non-existant file
+  @Test
+  public void readFile_test1(){
+
+    //create a Deliverable4 object
+    Deliverable4 d4 = new Deliverable4();
+
+    //force the print output to come to the outContent stream
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+
+    //call the hashIt function with a non-existant file
+    d4.readFile("e.txt");
+    // assertEquals(outContent.toString(), "Unable to find the file: e.txt");
+
+    assertTrue(outContent.toString().contains("Unable to find the file:"));
+
+  }
+
+  // Test the readFile function with a the test file which is appropriate input
+  @Test
+  public void readFile_test2(){
+
+    //create a Deliverable4 object
+    Deliverable4 d4 = new Deliverable4();
+
+    List<String> theFile = new ArrayList<String>();
+    theFile.add("tsar");
+
+    //call the hashIt function with a misformatted file (specifically does not have any :'s in it)
+    assertEquals(theFile, d4.readFile("test.txt"));
+
+  }
+
+  //Test readfile
 
 
 }
